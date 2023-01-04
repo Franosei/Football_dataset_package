e4bf7939e3604f59ab6c367d490f3d0fed6aabe0
Francis Osei <92299789+Franosei@users.noreply.github.com>
2023-01-04 03:19:13 +0100

    commit

--- leagueCR7/R/leagueCR7.R
+++ leagueCR7/R/leagueCR7.R
@@ -1,6 +1,6 @@
-#' League Matches Accross the World
+#' League Matches Across the World
 #'
-#' TThis is R data package that contains more than 150000
+#' This is a data package that contains more than 150000
 #' historical world football matches between 2018 and 2022,
 #' with more than 860 leagues and 9500 teams.
 #' @docType package
@@ -10,13 +10,13 @@ NULL
 
 #' "leagueCR7"
 #'
-#' A dataset upload in Kaggle  for Football Match Probability Prediction
+#'
 #'
 #'
 #' @source \url{https://www.kaggle.com/competitions/football-match-probability-prediction/data}
-#' @format A dataframe The data set contains more than 110938 historical world football matches
+#' @format The dataset 110938 historical world football matches
 #' \describe{
-#'   \item{target}{The outcome of a match 0: Home team lost,1 Draw and, 2 Home team won }
+#'   \item{target}{The outcome of a match 0: Home team lost,1: Draw and, 2: Home team won }
 #'   \item{home_team_name}{The name of the Home the team}
 #'   \item{league_name}{The league name}
 #'   \item{is_cup}{If the value is 1 the match is played for a cup compettion.}
