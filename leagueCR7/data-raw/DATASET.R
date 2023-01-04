ea0e96dcc888a0407cd8a6f4a1caaeea2ba9a1b3
Francis Osei <92299789+Franosei@users.noreply.github.com>
2023-01-04 03:51:37 +0100

    commit

--- leagueCR7/data-raw/DATASET.R
+++ leagueCR7/data-raw/DATASET.R
@@ -3,5 +3,8 @@ library(tidyverse)
 leagueCR7 <-
   readr::read_csv("cleandata.csv") %>%
   janitor::clean_names()
-
+leagueCR7$target <- as.factor(leagueCR7$target)
+leagueCR7$home_team_name <- as.factor(leagueCR7$home_team_name)
+leagueCR7$league_name <- as.factor(leagueCR7$league_name)
+leagueCR7$is_cup <- as.factor(leagueCR7$is_cup)
 usethis::use_data(leagueCR7)
